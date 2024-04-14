88fa8d7764a7b5ec20b826c27cc8414caa054211
Amir Hossein <amir73khakbaz@gmail.com>
2024-04-14 15:17:03 +0330

    implement test by install package

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {SharedStateProvider,useSharedSimpleState} from "sharedsimplestate"
 function App() {
+  const [data, setData] = useSharedSimpleState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SharedStateProvider>
+      <div>{data}</div>
+      <button onClick={(e) => setData(data + 1)}>increment</button>
+    </SharedStateProvider>
   );
 }
 
